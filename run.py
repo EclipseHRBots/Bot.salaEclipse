d776205e859f9cb181ba9b9cf623e80ca2cefbe1
EclipseHRBots <zzuxx81@gmail.com>
2024-12-09 19:55:15 -0400

    run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class WebServer():
 
 
 class RunBot():
-  room_id = "675783040f4456f042d03d41"
+  room_id = "6730115f0e0721fda0af4972"
   bot_token = "497761234868f1f869f8ae5400f668efaffa3ccbbcf4b5ef16d2f7e7991ab2b7"
   bot_file = "main"
   bot_class = "Bot"
