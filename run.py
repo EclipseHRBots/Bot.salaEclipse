9658164df7f97118f186a6376515f02db469346d
BOT Nezux <zzuxx81@gmail.com>
2024-12-10 14:47:31 -0400

    run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class WebServer():
 
 
 class RunBot():
-  room_id = "664ba3bd24cb5a1b8d8c741c"
+  room_id = "66779cfbd710bc3f2878aa31"
   bot_token = "497761234868f1f869f8ae5400f668efaffa3ccbbcf4b5ef16d2f7e7991ab2b7"
   bot_file = "main"
   bot_class = "Bot"
